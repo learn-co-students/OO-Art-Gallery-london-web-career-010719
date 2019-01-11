5a65dbd5ad70496201ccb11037b35bb76a1b3c5d
joeyhere <josephodell@outlook.com>
2019-01-11 18:04:27 +0000

    working

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,39 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    galleries.map {|gallery| gallery.city}.uniq
+  end
+
+  def self.total_experience
+    self.all.inject(0){|sum, artist| sum += artist.years_experience}
+  end
+
+  def self.most_prolific
+    self.all.max_by {|artist| artist.paintings.length / artist.years_experience}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, gallery, self)
+  end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,24 @@
+
 class Painting
 
+  attr_accessor :artist, :gallery
   attr_reader :title, :price
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, gallery, artist)
     @title = title
     @price = price
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    self.all.inject(0){|sum, painting| sum += painting.price}
+  end
+
+  puts "hello"
 end
