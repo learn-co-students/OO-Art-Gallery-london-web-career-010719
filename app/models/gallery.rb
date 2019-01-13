27dc1a2ea54700b58457ce39a0d4d0c4e3b7aac4
kkoomin <minha.koo.9@gmail.com>
2019-01-13 21:11:19 +0000

    Challenge Solved

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,27 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists
+    paintings.collect{|painting| painting.artist}.uniq
+  end
+
+  def artist_names
+    artists.collect{|artist| artist.name}.uniq
+  end
+
+  def most_expensive_painting
+    paintings.sort_by{|painting| painting.price}[-1]
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,3 +6,20 @@ require_relative '../config/environment.rb'
 binding.pry
 
 puts "Bob Ross rules."
+
+
+
+# X = Gallery.new("Hoam", "Seoul")
+# Y = Gallery.new("National", "London")
+# a = Painting.new("Apple", 100, X)
+# b = Painting.new("Bear", 200, Y)
+# c = Painting.new("Cider", 300, X)
+# d = Painting.new("Doby", 150, Y)
+# A = Artist.new("John", 5)
+# B = Artist.new("Chris", 7)
+#
+#
+# a.artist = A
+# b.artist = B
+# c.artist = A
+# d.artist = B
