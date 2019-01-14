8e9b59218e630c4538b7940b565c1e06a8760458
github username <github email address>
2019-01-14 12:49:55 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,58 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
-  def initialize(name, years_experience)
-    @name = name
-    @years_experience = years_experience
-  end
+      def initialize(name, years_experience)
+        @name = name
+        @years_experience = years_experience
+        @@all << self
+      end
+
+      def self.all
+        @@all
+      end
+
+      def paintings
+          Painting.all.select {|painting| painting.artist == self }
+      end
+
+
+      def galleries
+        paintings.map do | painting|
+            painting.gallery
+        end
+      end
+
+      def cities
+        galleries.map do |gallery|
+            gallery.city
+        end
+      end
+
+      def self.experience
+           @@all.map {|artist| artist.years_experience }
+      end
+
+      def self.total_experience
+        # experience = @@all.map {|artist| artist.years_experience }
+        experience.inject {|total, num| total + num }
+
+      end
+
+      def self.most_prolific
+
+          @@all.max_by do |artist|
+
+          artist.paintings.count.to_f/artist.years_experience.to_f
+
+          end
+
+      end
+
+      def create_painting(title,price,gallery)
+        Painting.new(title,price,self,gallery)
+      end
+
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,31 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist
+  attr_accessor :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
-  end
+    @artist = artist
+    @gallery = gallery
+    @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+
+
+    def self.total_price
+        price = @@all.map do | painting|
+            painting.price
+        end
+
+        price.inject {|total, n| total + n}.to_i
+    end
+
 
 end
